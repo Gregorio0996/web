68f2c423b591722ac3cebe41240caa4721a023b9
Gregorio1809 <gregorioimanuel@gmail.com>
2018-09-10 08:43:15 +0700

    adding more details

--- music/models.py
+++ music/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import Permission, User
 from django.urls import reverse
 
 
 class Album(models.Model):
+    #users = models.ForeignKey(User, default=0)
     artis = models.CharField(max_length=250)
     album_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
@@ -20,6 +22,7 @@ class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
+    audio_file = models.FileField(default='')
     favorite_is = models.BooleanField(default=False)
 
     def __str__(self):
